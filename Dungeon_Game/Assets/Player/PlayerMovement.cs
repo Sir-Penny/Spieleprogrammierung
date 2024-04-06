d5796af9c97adcea82ab580eedaf5183aa0d8974
sogaralles <92931752+sogaralles@users.noreply.github.com>
2024-04-06 18:02:20 +0200

    Animations added

--- Dungeon_Game/Assets/Player/PlayerMovement.cs
+++ Dungeon_Game/Assets/Player/PlayerMovement.cs
@@ -9,29 +9,51 @@ public class PlayerMovement : MonoBehaviour
     public Camera playerCame;
     public GameObject Fireball;
     public GameObject spellCastPoint;
+    private Animator animator;
+
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = gameObject.transform.GetChild(0).GetComponent<Animator>();
     }
     void Update()
     {
 
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
-            {
-                Vector3 lookPos = hit.point - transform.position;
-                lookPos.y = 0;
-                Quaternion rotation = Quaternion.LookRotation(lookPos);
-                transform.rotation = rotation; 
-            }
-        if (Input.GetMouseButton(0))
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 lookPos = hit.point - transform.position;
+            lookPos.y = 0;
+            Quaternion rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = rotation;
+        }
+        print(navMeshAgent.isStopped);
+        
+        if (navMeshAgent.remainingDistance < 0.1)
+        {
+            animator.SetBool("WalkingPlayer", false);
+            //print("WalkingPlayer false");
+        }
+        else
+        {
+            animator.SetBool("WalkingPlayer", true);
+            //print("WalkingPlayer true");
+        }
+            if (Input.GetMouseButton(0))
         {
             navMeshAgent.destination = hit.point;
         }
+
+        
         if (Input.GetKeyDown(KeyCode.Q))
         {
+            //navMeshAgent.speed = 0;
+            //navMeshAgent.isStopped = true;
+            //animator.SetBool("WalkingPlayer", false);
+            animator.SetTrigger("Throwing");
+            print("Throw true");
             Instantiate(Fireball, spellCastPoint.transform.position, spellCastPoint.transform.rotation);
         }
     }
