3b259343b41e9cf07fbab1b02333ca25cfc650a5
Sir-Penny <92923661+Sir-Penny@users.noreply.github.com>
2024-06-26 22:30:20 +0200

    Exp Bar

--- Dungeon_Game/Assets/Player/PlayerExp.cs
+++ Dungeon_Game/Assets/Player/PlayerExp.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerExp : MonoBehaviour
 {
-    public int exp=0;
-    public int maxexp;
+    public float exp=0;
+    public float maxexp;
+    public Slider sliderUI;
 
     public GameObject LevelUpWindow;
 
+
+    public void UpdateExpBar()
+    {
+        sliderUI.value = exp / maxexp;
+    }
     public void ReciveExp(int ammount)
     {
         exp += ammount;
         if(exp >= maxexp)
         {
+            float overload = maxexp - exp;
             Time.timeScale = 0;
             LevelUpWindow.GetComponent<SkillUpgradeManager>().InitalizeUpgrades();
-            maxexp *=  2;
+            maxexp *=  1.5f;
+            exp = overload;
         }
+        UpdateExpBar();
     }
 }
